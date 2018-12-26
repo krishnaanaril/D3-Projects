e6e94d54004eff4152fe7dafc241d439371cf09a
Krishna Mohan A M <krishnamohan.a.m@gmail.com>
2018-12-26 17:25:45 +0530

    Chapter 2 changes with table sorting

--- Chapter2/index.js
+++ Chapter2/index.js
@@ -1,4 +1,45 @@
-d3.select('strong')
-    .style('color', 'red')
-    .style('text-decoration', 'underline')
-    .style('cursor', 'pointer');
+async function lifeExpectancyTable() {
+    console.log('in method')
+    const getData = async () => {
+        try {
+            const response = await fetch('/data/who-gho-life-expectancy.json');
+            const raw = await response.json();
+            return raw.fact.filter(d => d.dims.GHO === 'Life expectancy at birth (years)'
+                && d.dims.SEX === 'Both sexes' && d.dims.YEAR === '2016')
+                .map(d => [
+                    d.dims.REGION,
+                    d.Value,
+                ]);
+        } catch (e) {
+            console.error(e);
+            return undefined;
+        }
+    };
+    const data = await getData();
+    data.unshift(['Region', 'Life expectancy (years from birth)']);
+    return tableFactory(data)
+        .table
+        .selectAll('tr')
+        .filter(i => i)
+        .sort(([a, b], [c, d]) => {
+            // sorting by descending
+            return d - b;
+        });
+}
+
+lifeExpectancyTable();
+
+const protoChart = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    margin: {
+        left: 10,
+        right: 10,
+        top: 10,
+        bottom: 10,
+    },
+};
+// setInterval(()=>{    
+//     console.log('pushing values...');
+//     rows.push(['a', 'b', 'c', 'd', 'e', 'f']);
+// }, 3000);
---
+++ Chapter2/table-factory.js
@@ -0,0 +1,34 @@
+function tableFactory(_rows) {
+    const rows = Array.from(_rows);
+    const header = rows.shift();
+    // const data = rows;
+
+    const table = d3.select('body')
+        .append('table')
+        .attr('class', 'table');
+
+    table.append('thead')
+        .append('tr')
+        .selectAll('td')
+        .data(header)
+        .enter()
+        .append('th')
+        .text(d => d);
+
+    table.append('tbody')
+        .selectAll('tr')
+        .data(rows)
+        .enter()
+        .append('tr')
+        .selectAll('td')
+        .data(d => d)
+        .enter()
+        .append('td')
+        .text(d => d);
+        
+    return {
+        table,
+        header,
+        rows,
+    };
+}
