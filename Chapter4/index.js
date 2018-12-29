87d9a3978b33c0fdfb0805cfa05f52399bb28d54
krishnaanaril <krishnamohan.a.m@gmail.com>
2018-12-29 19:40:55 +0530

    Need to do the geo tutorial

--- Chapter4/index.js
+++ Chapter4/index.js
@@ -15,7 +15,11 @@ const scalesDemo = (enabled => {
     }
 })(false); // set to true to see this stuff
 
-const chart = chartFactory();
+const chartSVG = ((enabled) => {
+    if (!enabled)
+        return;
+    const chart = chartFactory();
+})(false);
 
 (function ordinalScales(enabled) {
     if (enabled) {
@@ -178,3 +182,141 @@ const chart = chartFactory();
     }
 }(false));
 
+// Section flowcontrol generators and promise
+const UlamSpiral = ((enabled) => {
+    if (!enabled)
+        return;
+    const chart = chartFactory();
+
+    const generateSpiral = (n) => {
+        const spiral = [];
+        let x = 0;
+        let y = 0;
+        const min = [0, 0];
+        const max = [0, 0];
+        let add = [0, 0];
+        let direction = 0;
+        const directions = {
+            up: [0, -1],
+            left: [-1, 0],
+            down: [0, 1],
+            right: [1, 0],
+        };
+
+        d3.range(1, n).forEach((i) => {
+            spiral.push({ x, y, n: i });
+            add = directions[['up', 'left', 'down', 'right'][direction]];
+            x += add[0];
+            y += add[1];
+            if (x < min[0]) {
+                direction = (direction + 1) % 4;
+                min[0] = x;
+            }
+            if (x > max[0]) {
+                direction = (direction + 1) % 4;
+                max[0] = x;
+            }
+            if (y < min[1]) {
+                direction = (direction + 1) % 4;
+                min[1] = y;
+            }
+            if (y > max[1]) {
+                direction = (direction + 1) % 4;
+                max[1] = y;
+            }
+        });
+        return spiral;
+    }
+
+    const dot = d3.symbol().type(d3.symbolCircle).size(3);
+    const center = window.innerHeight / 2;
+    const l = 2;
+    const x = (x, l) => center + (l * x);
+    const y = (y, l) => center + (l * y);
+
+    function generatePrimes(n) {
+
+        function* numbers(start) {
+            while (true) {
+                yield start++;
+            }
+        }
+
+        function* filter(seq, prime) {
+            for (let num of seq) {
+                if (num % prime !== 0) {
+                    yield num;
+                }
+            }
+        }
+
+        function* primes() {
+            var seq = numbers(2); // Start on 2.
+            var prime;
+            while (true) {
+                prime = seq.next().value;
+                yield prime;
+                seq = filter(seq, prime);
+            }
+        }
+
+        function* getPrimes(count, seq) {
+            while (count) {
+                yield seq.next().value;
+                count--;
+            }
+        }
+
+        let results = [];
+        for (let prime of getPrimes(n, primes())) {
+            results.push(prime);
+        }
+
+        return results;
+    }
+
+    const primes = generatePrimes(2000);
+    const sequence = generateSpiral(d3.max(primes)).filter(d =>
+        primes.indexOf(d.n) > -1);
+
+    chart.container.selectAll('path')
+        .data(sequence)
+        .enter()
+        .append('path')
+        .attr('transform', d => `translate(${x(d.x, l)}, ${y(d.y, l)})`)
+        .attr('d', dot);
+
+    const scale = 8;
+    const regions = d3.nest()
+        .key(d => Math.floor(d.x / scale))
+        .key(d => Math.floor(d.y / scale))
+        .rollup(d => d.length)
+        .map(sequence);
+
+    const values = d3.merge(d3.keys(regions)
+        .map(_x => d3.values(regions[_x])));
+    const median = d3.median(values);
+    const extent = d3.extent(values);
+    const shades = (extent[1] - extent[0]) / 2;
+
+    regions.each((_x, _xKey) => {
+        _x.each((_y, _yKey) => {
+            let color,
+                red = '#e23c22',
+                green = '#497c36';
+            if (_y > median) {
+                color = d3.rgb(green).brighter(_y / shades);
+            } else {
+                color = d3.rgb(red).darker(_y / shades);
+            }
+            chart.container.append('rect')
+                .attr('x', x(_xKey, l * scale))
+                .attr('y', y(_yKey, l * scale))
+                .attr('width', l * scale)
+                .attr('height', l * scale)
+                .style('fill', color)
+                .style('fill-opacity', 0.9);
+        });
+    });
+
+})(false);
